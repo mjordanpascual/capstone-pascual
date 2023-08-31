fcdfbce06c642c3e2dbba58ac3248c1e7c3ee08b
@mjordanpascual <mjordan.pascual@gmail.com>
2023-08-31 12:26:18 +0800

    Update Codes

---
+++ script.js
@@ -0,0 +1,9 @@
+// type js
+const typed = new Typed('.multiple-text', {
+    strings: ['Front-End Developer', 'Back-End Developer', 'Full-Stack Developer'],
+    typeSpeed: 100,
+    backSpeed: 30,
+    backDelay: 200,
+    loop: true
+});
+
